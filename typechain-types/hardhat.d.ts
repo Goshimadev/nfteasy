9fa7eb6408c95dc75a757df20ee2f650ce3578cb
velopace <velopace@outlook.com>
2022-04-29 12:20:27 -0500

    Add ERC721Tradable and meta-transaction

--- typechain-types/hardhat.d.ts
+++ typechain-types/hardhat.d.ts
@@ -13,13 +13,29 @@ import * as Contracts from ".";
 declare module "hardhat/types/runtime" {
   interface HardhatEthersHelpers extends HardhatEthersHelpersBase {
     getContractFactory(
+      name: "Ownable",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.Ownable__factory>;
+    getContractFactory(
       name: "ERC721",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC721__factory>;
     getContractFactory(
+      name: "ERC721Burnable",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ERC721Burnable__factory>;
+    getContractFactory(
+      name: "ERC721Enumerable",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ERC721Enumerable__factory>;
+    getContractFactory(
       name: "ERC721URIStorage",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC721URIStorage__factory>;
+    getContractFactory(
+      name: "IERC721Enumerable",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.IERC721Enumerable__factory>;
     getContractFactory(
       name: "IERC721Metadata",
       signerOrOptions?: ethers.Signer | FactoryOptions
@@ -44,17 +60,53 @@ declare module "hardhat/types/runtime" {
       name: "Babel",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Babel__factory>;
+    getContractFactory(
+      name: "EIP712Base",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.EIP712Base__factory>;
+    getContractFactory(
+      name: "NativeMetaTransaction",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.NativeMetaTransaction__factory>;
+    getContractFactory(
+      name: "ERC721Tradable",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ERC721Tradable__factory>;
+    getContractFactory(
+      name: "ProxyRegistry",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.ProxyRegistry__factory>;
 
+    getContractAt(
+      name: "Ownable",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Ownable>;
     getContractAt(
       name: "ERC721",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.ERC721>;
     getContractAt(
+      name: "ERC721Burnable",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC721Burnable>;
+    getContractAt(
+      name: "ERC721Enumerable",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC721Enumerable>;
+    getContractAt(
       name: "ERC721URIStorage",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.ERC721URIStorage>;
+    getContractAt(
+      name: "IERC721Enumerable",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC721Enumerable>;
     getContractAt(
       name: "IERC721Metadata",
       address: string,
@@ -85,6 +137,26 @@ declare module "hardhat/types/runtime" {
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.Babel>;
+    getContractAt(
+      name: "EIP712Base",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.EIP712Base>;
+    getContractAt(
+      name: "NativeMetaTransaction",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.NativeMetaTransaction>;
+    getContractAt(
+      name: "ERC721Tradable",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC721Tradable>;
+    getContractAt(
+      name: "ProxyRegistry",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ProxyRegistry>;
 
     // default types
     getContractFactory(
